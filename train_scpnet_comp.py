f6226b95beed7343b11c6b1ceeabf5f5d670692c
ngoductuanlhp <ngoductuanlhp@gmail.com>
2023-05-26 11:51:34 +0700

    # refactor code

--- train_scpnet_comp.py
+++ train_scpnet_comp.py
@@ -89,6 +89,105 @@ def load_dataset(config='config/bev.py'):
 def pass_print(*args, **kwargs):
     pass
 
+
+def train(my_model, optimizer, train_dataset_loader, lovasz_softmax, loss_func, model_save_path, epoch, ignore_label):
+    pytorch_device = torch.device('cuda')
+    
+    loss_list = []
+    pbar = tqdm(total=len(train_dataset_loader))
+    time.sleep(10)
+    # lr_scheduler.step(epoch)
+    
+    my_model.train()
+    for i_iter, (_, train_vox_label, train_grid, _, train_pt_fea, train_index, origin_len) in enumerate(train_dataset_loader):
+        #train_vox_label: 1x256x256x32
+        #train_grid[0]: Nx3 int32 [-40, 40]
+        #grain_pt_fea[0]: Nx7 float64
+            
+        train_pt_fea_ten = [torch.from_numpy(i).type(torch.FloatTensor).to(pytorch_device) for i in train_pt_fea]
+        train_vox_ten = [torch.from_numpy(i).to(pytorch_device) for i in train_grid]
+        point_label_tensor = train_vox_label.type(torch.LongTensor).to(pytorch_device)
+
+        
+        # forward + backward + optimize
+        outputs = my_model(train_pt_fea_ten, train_vox_ten, point_label_tensor.shape[0])
+        #breakpoint()
+        loss = lovasz_softmax(torch.nn.functional.softmax(outputs), point_label_tensor, ignore=ignore_label) + loss_func(
+            outputs, point_label_tensor)
+        loss.backward()
+        optimizer.step()
+        loss_list.append(loss.item())
+
+        if i_iter % 100 == 0:
+            if len(loss_list) > 0:
+                print('epoch %d iter %5d, loss: %.3f\n' %
+                        (epoch, i_iter, np.mean(loss_list)))
+            else:
+                print('loss error')
+
+        optimizer.zero_grad()
+        pbar.update(1)
+        # global_iter += 1
+    pbar.close()
+
+    model_save_name = model_save_path + ('epoch%d.pth' % (epoch))
+    torch.save(my_model.state_dict(), model_save_name)
+    
+
+def val(my_model, dataset, val_dataset_loader, lovasz_softmax, loss_func, val_batch_size, ignore_label, occ_type):
+    pytorch_device = torch.device('cuda')
+
+    my_model.eval()
+    with torch.no_grad():
+        dataset.occ_eval_metrics = Metric_mIoU(num_classes=18,
+                                            use_lidar_mask=True,
+                                            use_image_mask=False)
+
+        show_dir = "./pred/baseline_nuscenes/"
+        if not os.path.exists(show_dir):
+            os.makedirs(show_dir)
+
+        for i_iter_val, (_, val_vox_label, val_grid, _, val_pt_fea, val_index, info) in enumerate(val_dataset_loader):
+            val_pt_fea_ten = [torch.from_numpy(i).type(torch.FloatTensor).to(pytorch_device) for i in val_pt_fea]
+            val_grid_ten = [torch.from_numpy(i).to(pytorch_device) for i in val_grid]
+            for bat in range(val_batch_size):
+                val_label_tensor = val_vox_label[bat,:].type(torch.LongTensor).to(pytorch_device)
+                val_label_tensor = torch.unsqueeze(val_label_tensor, 0)
+                predict_labels = my_model(val_pt_fea_ten, val_grid_ten, val_batch_size)
+                loss = lovasz_softmax(torch.nn.functional.softmax(predict_labels).detach(), val_label_tensor,
+                                                ignore=ignore_label) + loss_func(predict_labels.detach(), val_label_tensor)
+
+                predict_labels = torch.argmax(predict_labels, dim=1)
+                predict_labels = predict_labels.cpu().detach().numpy()
+                predict_labels = np.squeeze(predict_labels)
+                val_vox_label0 = val_vox_label[bat, :].cpu().detach().numpy()
+                val_vox_label0 = np.squeeze(val_vox_label0)
+                
+                #info = dataset.data_infos[val_index[0]]
+                if occ_type == 'mini':
+                    occ_gt = np.load(f"./data/occ3d-nus-{occ_type}/{info[0]['occ_gt_path']}")
+                else:
+                    occ_gt = np.load(f"./data/occ3d-nus/{info[0]['occ_gt_path']}")
+                
+                gt_semantics = occ_gt['semantics']
+                mask_lidar = occ_gt['mask_lidar'].astype(bool)
+                mask_camera = occ_gt['mask_camera'].astype(bool)
+                dataset.occ_eval_metrics.add_batch(predict_labels, val_vox_label0, mask_lidar, mask_camera)   
+                
+                
+                # vis = True
+                # if vis:
+                #     if val_index[0] % 10 == 0:     
+                #         sample_token=info[0]['token']
+                #         save_path=os.path.join(show_dir,str(val_index[0]).zfill(4))
+                #         np.savez_compressed(save_path,pred=predict_labels,gt=gt_semantics,gt_train=val_vox_label0,sample_token=sample_token)                  
+        
+        print('Validation per class iou: ')
+        del val_vox_label, val_grid, val_pt_fea, val_pt_fea_ten, val_grid_ten, val_label_tensor
+        print('\nStarting Evaluation...')
+        dataset.occ_eval_metrics.count_miou() 
+            
+
 def main(local_rank, args):
     torch.backends.cudnn.benchmark = True
     pytorch_device = torch.device('cuda')
@@ -119,14 +218,6 @@ def main(local_rank, args):
     SemKITTI_label_name = get_SemKITTI_label_name(dataset_config["label_mapping"])
     unique_label = np.asarray(sorted(list(SemKITTI_label_name.keys())))[1:] - 1
     unique_label_str = [SemKITTI_label_name[x] for x in unique_label + 1]
-    
-
-    # def init_dist(backend="nccl", **kwargs):
-    #     rank = int(os.environ["RANK"])
-    #     # world_size = int(os.environ["WORLD_SIZE"])
-    #     num_gpus = torch.cuda.device_count()
-    #     torch.cuda.set_device(rank % num_gpus)
-    #     dist.init_process_group(backend=backend, **kwargs)
 
     # init DDP
     distributed = True
@@ -192,7 +283,6 @@ def main(local_rank, args):
     #train_dataset_loader, val_dataset_loader, val_pt_dataset = tpvformer_data_builder.build()
     
     # training
-    epoch = 0
     best_val_miou = 0
     my_model.train()
     global_iter = 0
@@ -207,102 +297,9 @@ def main(local_rank, args):
     #load occ dataset
     dataset = load_dataset()
     
-    while epoch < train_hypers['max_num_epochs']:
-        loss_list = []
-        pbar = tqdm(total=len(train_dataset_loader))
-        time.sleep(10)
-        # lr_scheduler.step(epoch)
-        
-        my_model.train()
-        for i_iter, (_, train_vox_label, train_grid, _, train_pt_fea, train_index, origin_len) in enumerate(train_dataset_loader):
-            #train_vox_label: 1x256x256x32
-            #train_grid[0]: Nx3 int32 [-40, 40]
-            #grain_pt_fea[0]: Nx7 float64
-             
-            train_pt_fea_ten = [torch.from_numpy(i).type(torch.FloatTensor).to(pytorch_device) for i in train_pt_fea]
-            train_vox_ten = [torch.from_numpy(i).to(pytorch_device) for i in train_grid]
-            point_label_tensor = train_vox_label.type(torch.LongTensor).to(pytorch_device)
-
-            
-            # forward + backward + optimize
-            outputs = my_model(train_pt_fea_ten, train_vox_ten, point_label_tensor.shape[0])
-            #breakpoint()
-            loss = lovasz_softmax(torch.nn.functional.softmax(outputs), point_label_tensor, ignore=ignore_label) + loss_func(
-                outputs, point_label_tensor)
-            loss.backward()
-            optimizer.step()
-            loss_list.append(loss.item())
-
-            if global_iter % 1000 == 0:
-                if len(loss_list) > 0:
-                    print('epoch %d iter %5d, loss: %.3f\n' %
-                          (epoch, i_iter, np.mean(loss_list)))
-                else:
-                    print('loss error')
-
-            optimizer.zero_grad()
-            pbar.update(1)
-            global_iter += 1
-            if global_iter % check_iter == 0:
-                if len(loss_list) > 0:
-                    print('epoch %d iter %5d, loss: %.3f\n' %
-                          (epoch, i_iter, np.mean(loss_list)))
-                else:
-                    print('loss error')
-        
-            if global_iter % check_iter == 0:
-                my_model.eval()
-                with torch.no_grad():
-                    dataset.occ_eval_metrics = Metric_mIoU(num_classes=18,
-                                                        use_lidar_mask=True,
-                                                        use_image_mask=False)
-                    for i_iter_val, (_, val_vox_label, val_grid, _, val_pt_fea, val_index, info) in enumerate(val_dataset_loader):
-                        val_pt_fea_ten = [torch.from_numpy(i).type(torch.FloatTensor).to(pytorch_device) for i in val_pt_fea]
-                        val_grid_ten = [torch.from_numpy(i).to(pytorch_device) for i in val_grid]
-                        for bat in range(val_batch_size):
-                            val_label_tensor = val_vox_label[bat,:].type(torch.LongTensor).to(pytorch_device)
-                            val_label_tensor = torch.unsqueeze(val_label_tensor, 0)
-                            predict_labels = my_model(val_pt_fea_ten, val_grid_ten, val_batch_size)
-                            loss = lovasz_softmax(torch.nn.functional.softmax(predict_labels).detach(), val_label_tensor,
-                                                            ignore=ignore_label) + loss_func(predict_labels.detach(), val_label_tensor)
-
-                            predict_labels = torch.argmax(predict_labels, dim=1)
-                            predict_labels = predict_labels.cpu().detach().numpy()
-                            predict_labels = np.squeeze(predict_labels)
-                            val_vox_label0 = val_vox_label[bat, :].cpu().detach().numpy()
-                            val_vox_label0 = np.squeeze(val_vox_label0)
-                            
-                            #info = dataset.data_infos[val_index[0]]
-                            occ_gt = np.load(f"./data/occ3d-nus-{occ_type}/{info[0]['occ_gt_path']}")
-                            
-                            gt_semantics = occ_gt['semantics']
-                            mask_lidar = occ_gt['mask_lidar'].astype(bool)
-                            mask_camera = occ_gt['mask_camera'].astype(bool)
-                            dataset.occ_eval_metrics.add_batch(predict_labels, val_vox_label0, mask_lidar, mask_camera)   
-                            vis = True
-                            show_dir = "./pred/no_shuffle_2/"
-
-                            if not os.exists(show_dir):
-                                os.makedirs((show_dir))
-
-                            # if vis:
-                            #     if val_index[0] % 10 == 0:     
-                            #         sample_token=info[0]['token']
-                            #         save_path=os.path.join(show_dir,str(val_index[0]).zfill(4))
-                            #         np.savez_compressed(save_path,pred=predict_labels,gt=gt_semantics,gt_train=val_vox_label0,sample_token=sample_token)                  
-                    
-                    print('Validation per class iou: ')
-                    del val_vox_label, val_grid, val_pt_fea, val_pt_fea_ten, val_grid_ten, val_label_tensor
-                    print('\nStarting Evaluation...')
-                    dataset.occ_eval_metrics.count_miou() 
-                    
-                    model_save_name = model_save_path + ('epoch%d.pth' % (epoch))
-                    torch.save(my_model.state_dict(), model_save_name)
-                my_model.train()
-        pbar.close()
-        
-        
-        epoch += 1
+    for epoch in range(train_hypers['max_num_epochs']):
+        val(my_model, dataset, val_dataset_loader, lovasz_softmax, loss_func, val_batch_size, ignore_label, occ_type)
+        train(my_model, optimizer, train_dataset_loader, lovasz_softmax, loss_func, model_save_path, epoch, ignore_label)
 
 
 if __name__ == '__main__':
